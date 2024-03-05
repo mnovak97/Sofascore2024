a12cce8e717d3430127c1c756ad093f397659106
mnovak97 <63095399+mnovak97@users.noreply.github.com>
2024-03-05 13:00:38 +0100

    Initial commit

---
+++ Sofascore/Sofascore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sofascore
+//
+//  Created by Martin Novak on 05.03.2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
